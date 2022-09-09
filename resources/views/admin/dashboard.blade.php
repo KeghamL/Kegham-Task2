7bb535968405aede887471960f5a9d00e773be01
Erik <gevorgyane699@gmail.com>
2022-09-09 16:34:11 +0400

    new

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -129,34 +129,32 @@
                 <div class="max-w-7x1 mx-auto sm:px-6 lg:px-8">
                     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 bg-white border-b border-gray-200">
-                            @if (!empty($unreadNotifications))
-                                @forelse (auth()->user()->unreadNotifications as $notification)
-                                    @if ($notification->type == 'App\Notifications\BrandNewNotification')
-                                        <div class="bg-light text-dark  p-2 m-3 ">
-                                            <b>{{ $notification->data['fname'] }}
-                                                ({{ $notification->data['email'] }})
-                                    @endif
-                                    </b>
-                                    Registered In
-                                    Your
-                                    Website!!
-                                    <a href="{{ route('markasread', $notification->id) }}"
-                                        class="p-2 bg-red-400 text-danger rounded-lg">MarkAsRead</a>
-                                @elseif ($notification->type == 'App\Notifications\MessageNewNotification')
+                            @forelse (auth()->user()->unreadNotifications as $notification)
+                                @if ($notification->type == 'App\Notifications\BrandNewNotification')
                                     <div class="bg-light text-dark  p-2 m-3 ">
-                                        You Have A New Message Send By
-                                        <b>({{ $notification->data['name'] }})
+                                        <b>{{ $notification->data['fname'] }}
+                                            ({{ $notification->data['email'] }})
                                         </b>
+                                        Registered In
+                                        Your
+                                        Website!!
                                         <a href="{{ route('markasread', $notification->id) }}"
                                             class="p-2 bg-red-400 text-danger rounded-lg">MarkAsRead</a>
-                                    @elseif ($notification->type == 'App\Notifications\PaymentNotification')
+                                    @elseif ($notification->type == 'App\Notifications\MessageNewNotification')
                                         <div class="bg-light text-dark  p-2 m-3 ">
-                                            User<b>({{ $notification->data['fullname'] }})</b> From
-                                            <b>({{ $notification->data['city'] }})
+                                            You Have A New Message Send By
+                                            <b>({{ $notification->data['name'] }})
                                             </b>
-                                            Has Purhcased A New Service!!
                                             <a href="{{ route('markasread', $notification->id) }}"
                                                 class="p-2 bg-red-400 text-danger rounded-lg">MarkAsRead</a>
+                                        @elseif ($notification->type == 'App\Notifications\PaymentNotification')
+                                            <div class="bg-light text-dark  p-2 m-3 ">
+                                                User<b>({{ $notification->data['fullname'] }})</b> From
+                                                <b>({{ $notification->data['city'] }})
+                                                </b>
+                                                Has Purhcased A New Service!!
+                                                <a href="{{ route('markasread', $notification->id) }}"
+                                                    class="p-2 bg-red-400 text-danger rounded-lg">MarkAsRead</a>
                                 @endif
 
                             @empty
